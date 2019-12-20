b8675649dfb11196d205c7046a65d7e65f9a9dd6
jackeado <pabsanchez@gmail.com>
2019-12-20 20:59:57 +0000

    fix

--- models/city.py
+++ models/city.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """This is the city class"""
+#!/usr/bin/python3                                                                                                                                                                                             
+"""This is the city class"""
 from models.base_model import BaseModel
+from sqlalchemy import Table, Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class City(BaseModel):
-    """This is the class for City
+    """This is the class for City                                                                                                                                                                             
     Attributes:
-        state_id: The state id
-        name: input name
+    state_id: The state id
+    name: input name                                                                                                                                                                                      \
     """
-    state_id = ""
-    name = ""
+    __tablename__ ="cities"
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    name = Column(String(128), nullable=False)
